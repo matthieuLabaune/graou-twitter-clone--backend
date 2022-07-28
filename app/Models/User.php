87a1b642d1b1bdc0f9d1f812eee3a97f708ab9be
Matthieu <matthieu.labaune@gmail.com>
2022-07-28 12:19:19 +0200

    Add register backend logic

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,15 +14,9 @@ class User extends Authenticatable
     use HasApiTokens, HasFactory, Notifiable;
 
     /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
+     * @var array
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $guarded = [];
 
     /**
      * The attributes that should be hidden for serialization.
